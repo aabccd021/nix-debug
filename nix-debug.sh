3135855ff3af66cbb1d779a85010d51de98bd92e
aabccd021 <aabccd021@gmail.com>
2025-02-08 21:25:33 +0700

    refactor: simplify nix-debug.sh script

--- nix-debug.sh
+++ nix-debug.sh
@@ -16,42 +16,10 @@ if [ -z "$target" ]; then
   )
 fi
 
-nix derivation show ".#$target"
-
 outpath=$(nix derivation show ".#$target" | jq -r 'values[].env.out')
-
-echo "Outpath: $outpath"
-
-# if [ -e "$outpath" ]; then
-#   echo "-e $outpath"
-# fi
-#
-# if [ -d "$outpath" ]; then
-#   echo "-d $outpath"
-# fi
-#
-# if [ -f "$outpath" ]; then
-#   echo "-f $outpath"
-# fi
-#
-# if [ -L "$outpath" ]; then
-#   echo "-L $outpath"
-# fi
-#
-# echo "hmm"
-#
-# file "$outpath" || true
-# ls "$outpath" || true
-#
-# nix build --log-lines 0 --print-build-logs ".#$target"
-#
-echo "done"
-# if ; then
-#   echo "Not showing logs for $outpath"
-#   nix build --log-lines 0 --print-build-logs ".#$target"
-#   echo "Done"
-# else
-#   echo "Showing logs for $outpath"
-#   nix log ".#$target"
-#   echo "Done"
-# fi
+if [ -e "$outpath" ]; then
+  echo "Showing build log of .#$target"
+  nix log ".#$target"
+else
+  nix build --log-lines 0 --print-build-logs ".#$target"
+fi
