49d7fa2ea0b01e8e79a1b22490147fea12579b79
aabccd021 <aabccd021@gmail.com>
2025-02-08 21:20:23 +0700

    Update .gitignore and nix-debug.sh configurations

--- nix-debug.sh
+++ nix-debug.sh
@@ -1,3 +1,5 @@
+# set -eu
+
 trap 'cd $(pwd)' EXIT
 root=$(git rev-parse --show-toplevel)
 cd "$root" || exit
@@ -14,4 +16,42 @@ if [ -z "$target" ]; then
   )
 fi
 
-nix build --log-lines 0 --print-build-logs ".#$target"
+nix derivation show ".#$target"
+
+outpath=$(nix derivation show ".#$target" | jq -r 'values[].env.out')
+
+echo "Outpath: $outpath"
+
+# if [ -e "$outpath" ]; then
+#   echo "-e $outpath"
+# fi
+#
+# if [ -d "$outpath" ]; then
+#   echo "-d $outpath"
+# fi
+#
+# if [ -f "$outpath" ]; then
+#   echo "-f $outpath"
+# fi
+#
+# if [ -L "$outpath" ]; then
+#   echo "-L $outpath"
+# fi
+#
+# echo "hmm"
+#
+# file "$outpath" || true
+# ls "$outpath" || true
+#
+# nix build --log-lines 0 --print-build-logs ".#$target"
+#
+echo "done"
+# if ; then
+#   echo "Not showing logs for $outpath"
+#   nix build --log-lines 0 --print-build-logs ".#$target"
+#   echo "Done"
+# else
+#   echo "Showing logs for $outpath"
+#   nix log ".#$target"
+#   echo "Done"
+# fi
