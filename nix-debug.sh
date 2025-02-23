f1a884aceef9bb7eb394e89cc1393a9e72f75657
aabccd021 <aabccd021@gmail.com>
2025-02-23 20:01:23 +0700

    Update nix-debug.sh script for system detection and package selection.

--- nix-debug.sh
+++ nix-debug.sh
@@ -6,20 +6,23 @@ cd "$root" || exit
 git add -A
 trap 'git reset >/dev/null' EXIT
 
+system=$(nix eval --impure --raw --expr 'builtins.currentSystem')
+
 target=${1:-}
 
 if [ -z "$target" ]; then
-  target=$(
-    nix flake show --json |
-      jq --raw-output '.packages."x86_64-linux" | keys[]' |
-      fzf
-  )
+  packages=$(nix flake show --json | jq --raw-output ".packages.$system | keys[]")
+  if [ -z "$packages" ]; then
+    echo "No packages found for system $system"
+    exit 1
+  fi
+  target=$(echo "$packages" | fzf --prompt="Select a package: ")
 fi
 
 outpath=$(nix derivation show ".#$target" | jq -r 'values[].env.out')
 if [ -e "$outpath" ]; then
   echo "Showing build log of .#$target"
-  nix log ".#$target"
+  PAGER='' nix log ".#$target"
 else
   nix build --print-build-logs ".#$target"
 fi
