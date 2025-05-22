4ee254e227300b83a7c8b77f9c3bd03b3896ac72
aabccd021 <aabccd021@gmail.com>
2025-05-22 08:56:22 +0700

    refactor(nix-debug): Improved target and flags handling.

--- nix-debug.sh
+++ nix-debug.sh
@@ -1,4 +1,18 @@
-# set -eu
+target=""
+flags=""
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+  --target)
+    target="$2"
+    shift
+    ;;
+  *)
+    flags="$flags $1"
+    ;;
+  esac
+  shift
+done
 
 trap 'cd $(pwd)' EXIT
 root=$(git rev-parse --show-toplevel)
@@ -8,28 +22,24 @@ trap 'git reset >/dev/null' EXIT
 
 system=$(nix eval --impure --raw --expr 'builtins.currentSystem')
 
-target=${1:-}
-shift
-flags="$*"
-
 if [ -z "$target" ]; then
   packages=$(nix flake show --json | jq --raw-output ".packages.\"$system\" | keys[]")
   if [ -z "$packages" ]; then
     echo "No packages found for system $system"
     exit 1
   fi
-  target=$(echo "$packages" | fzf --prompt="Select a package: ")
+  target=$(echo "$packages" | sed "s|^|.#|" | fzf --prompt="Select a package: ")
 fi
 
 echo "nix-debug $target started"
 
-outpath=$(nix derivation show ".#$target" | jq -r 'values[].env.out')
+outpath=$(nix derivation show "$target" | jq -r 'values[].env.out')
 if [ -e "$outpath" ]; then
   # shellcheck disable=SC2086
-  PAGER='' nix log $flags ".#$target"
+  PAGER='' nix log $flags "$target"
 else
   # shellcheck disable=SC2086
-  nix build --print-build-logs $flags ".#$target"
+  nix build --print-build-logs $flags "$target"
 fi
 
-echo "nix-debug $target finished"
+echo "Finished \"nix-debug --target $target\""
